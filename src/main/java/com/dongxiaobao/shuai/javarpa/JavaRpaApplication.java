76a845ad447f9faabbed8c892a071752643e9702
dongxiaobao <dongyuefeng24k@foxmail.com>
2021-11-27 20:16:13 +0800

    init

---
+++ src/main/java/com/dongxiaobao/shuai/javarpa/JavaRpaApplication.java
@@ -0,0 +1,13 @@
+package com.dongxiaobao.shuai.javarpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongxiaobao/shuai/javarpa/JavaRpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongxiaobao.shuai.javarpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
